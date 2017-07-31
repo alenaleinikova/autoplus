3f9a439113b12d8b78553902ab3059f8081bb13d
alenaleinikova <alenu6k@gmail.com>
2017-07-31 16:38:43 +0300

    Update main.js

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,26 +1,7 @@
 
-/* Каталог в мобильной версии */
-
-/*const title = document.getElementsByClassName('.title');
-
-
-
-for ( var i = 0; i < title.length; i++) {
-  title[i].addEventListener('focus', function() {
-    if (!(this.classList.contains('active'))) {
-      for (var  i = 0; i < title.length; i++) {
-        title[i].classList.remove('active');
-      }
-    }
-  })
-}
-*/
-
 var burgerOn = document.querySelector('#burger-on'),
     burgerOff = document.querySelector('#burger-off'),
-    title = document.querySelectorAll('#title'),
-    promo = document.querySelector('#promo'),
-    header = document.querySelector('.menu');
+    title = document.querySelectorAll('#title');
 
 burgerOn.addEventListener('click', () => {
     burgerOn.classList.add('on');
@@ -29,19 +10,10 @@ burgerOff.addEventListener('click', () => {
     burgerOn.classList.remove('on');
 });
 
-promo.addEventListener('scroll', () => {
-   if( window.pageYOffset > 100 ) {
-     header.classList.add('on-scroll');
-   } else {
-     header.classList.remove('on-scroll');
-   }
- });
-
 
 for ( var i = 0; i < title.length; i++) {
   title[i].addEventListener('click', catalog )
 }
-
 
 
 function catalog () {
